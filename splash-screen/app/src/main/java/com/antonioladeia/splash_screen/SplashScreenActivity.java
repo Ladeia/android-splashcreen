08a66454e82bee0c382d52c37de5676edf22940c
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-04-06 16:41:56 -0300

    remove navigation bar of splash screen

--- splash-screen/app/src/main/java/com/antonioladeia/splash_screen/SplashScreenActivity.java
+++ splash-screen/app/src/main/java/com/antonioladeia/splash_screen/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
             @Override
@@ -19,8 +29,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 mostrarLogin();
             }
         }, 10000);
-
-        mostrarLogin();
     }
 
     private void mostrarLogin() {
